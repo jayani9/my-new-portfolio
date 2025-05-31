3b7ceccb03d9bfab6928aa3b570478f6f8327739
jayani9 <jayani135@gmail.com>
2025-05-31 20:41:18 +0300

    add files

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import { EDUCATION, PROJECTS } from '../constants'
+import { motion } from "framer-motion"
 
 const Education = () => {
-  return (
-    <div className='pb-4'>
-        <h1 className='my-20 text-center text-4xl font-bold text-purple-400'>Educa<span className='text-blue-400'>tion</span></h1>
-        <div>
-            {EDUCATION.map((element,index)=>(
-                <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-                    <div className='w-full max-w-xl lg:w-3/4'>
-                      <h6 className='mb-2 font-semibold'>{element.title}</h6>
-                      <p className='mb-4 text-neutral-400'>{element.description}</p>
-                      {element.technologies.map((tech,index)=>(
-                        <span key={index} className='mr-2 rounded bg-purple-400 px-2 py-1 text-sm font-medium text-purple-800'>{tech}</span>
-                      ))}
+    return (
+        <div className='pb-4'>
+            <h1 className='my-20 text-center text-4xl font-bold text-purple-400'>Educa<span className='text-blue-400'>tion</span></h1>
+            <div>
+                {EDUCATION.map((element, index) => (
+                    <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
+                        <motion.div initial={{ opacity: 0, x: 0 }} whileInView={{ opacity: 1, x: 20 }}
+                            transition={{ duration: 0.6, ease: "easeOut" }}
+                            viewport={{ once: false, amount: 0.2 }} className='w-full max-w-xl lg:w-3/4'>
+                            <h6 className='mb-2 font-semibold'>{element.title}</h6>
+                            <p className='mb-4 text-neutral-400'>{element.description}</p>
+                            {element.technologies.map((tech, index) => (
+                                <span key={index} className='mr-2 rounded bg-purple-400 px-2 py-1 text-sm font-medium text-purple-800'>{tech}</span>
+                            ))}
 
+                        </motion.div>
                     </div>
-                </div>
-                
-            ))}
+
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Education
